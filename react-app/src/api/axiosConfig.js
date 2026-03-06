5365a1f85c87c9b0eb2922fd7480aed9979707d6
norberto1995 <norbertomirandaguerra@gmail.com>
2026-03-05 22:24:44 -0500

    agregue la url del render

--- react-app/src/api/axiosConfig.js
+++ react-app/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gestioninventariospring-production.up.railway.app",
+  baseURL: "https://gestioninventariospring.onrender.com",
 });
 
 api.interceptors.request.use(

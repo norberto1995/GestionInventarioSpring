ae6b210a56536803b9011851de7ce12884d9065f
norberto1995 <norbertomirandaguerra@gmail.com>
2026-03-11 22:27:57 -0500

    cambie los datos para trabajar en local

--- react-app/src/api/axiosConfig.js
+++ react-app/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gestioninventariospring.onrender.com",
+  baseURL: "http://localhost:8080",
 });
 
 api.interceptors.request.use(
